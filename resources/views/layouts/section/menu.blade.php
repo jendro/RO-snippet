8059695856b64fc858bfd2d4d42fe56f9c0d5a5b
Jendro <jendropurnomo@gmail.com>
2019-07-21 06:42:11 +0700

    logout button

--- resources/views/layouts/section/menu.blade.php
+++ resources/views/layouts/section/menu.blade.php
@@ -4,7 +4,16 @@
     </li>
     <li><a href="{{ route('about') }}" class="default">About</a></li>
     @if(Auth::user())
-        <li><a href="{{ route('user.admin',['user'=>'nama_user']) }}" class="default">{{ Auth::user()->name }}</a></li>
+        <li class="dropdown">
+            <a class="default dropdown-toggle" type="button" data-toggle="dropdown">
+                {{ Auth::user()->name }}
+                <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+                <li><a href="{{ route('user.admin',['user'=>'nama_user']) }}">Profil</a></li>
+                <li><a href="{{ route('logout') }}">Logout</a></li>
+            </ul>
+        </li>
         <li>
             <a href="{{ route('snippet.add') }}" class="btn btn-block btn-social btn-tambah">
                 <i class="fa fa-plus"></i> Create Snippet
